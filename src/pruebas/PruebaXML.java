a90a6e4ad995c2935ccd15473b93f713e697b118
patodeath <patodeath@gmail.com>
2012-11-23 01:20:26 +0000

    Agregando el metodo equals y hashCode a la clase Atributo. Tambien agregue el método getTagHijoByAtributo a la clase Tag

--- src/pruebas/PruebaXML.java
+++ src/pruebas/PruebaXML.java
@@ -11,6 +11,7 @@ import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 import org.xml.sax.SAXException;
+import xml.analizador.dom.JespXML;
 import xml.analizador.dom.modelo.*;
 
 
@@ -30,15 +31,18 @@ public class PruebaXML {
      * @throws FileNotFoundException  
      */
     public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException, TransformerConfigurationException, FileNotFoundException, TransformerException {
-//        JespXML j = new JespXML("wea.xml");
-//        Tag raiz = new Tag("raiz");
-//        raiz.addAtributo("wea", "muol");
-//        j.escribirXML(raiz);
         
-        
-//        JespXML j = new JespXML("prueba.xml", Encoding.UTF_8);
-//        Tag raiz = j.leerXML();
-//        imprimirInformacion(raiz);
+            JespXML j = new JespXML("ayuda.xml");
+            Tag raiz = j.leerXML();
+//        
+        Tag t = raiz.getTagHijoByAtributo(new Atributo("id","if else"));
+        System.out.println(t);
+        //        Tag raiz = new Tag("raiz");
+        //        raiz.addAtributo("wea", "muol");
+        //        j.escribirXML(raiz);
+        //        JespXML j = new JespXML("prueba.xml", Encoding.UTF_8);
+        //        Tag raiz = j.leerXML();
+        //        imprimirInformacion(raiz);
     }
 
     /**
