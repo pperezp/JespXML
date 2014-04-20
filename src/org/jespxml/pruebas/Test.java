22839d61265ac13cec107ec2cf314884f95a42d5
patodeath <patodeath@gmail.com>
2014-04-20 18:33:33 +0000

    Creado los métodos escribir(Object objeto, Class clase) y leer(Class clase) para poder serializar un objeto completo a un archivo XML y poder leerlo de esa forma tambien.

--- src/org/jespxml/pruebas/Test.java
+++ src/org/jespxml/pruebas/Test.java
@@ -1,12 +1,10 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.jespxml.pruebas;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.xml.bind.JAXBException;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
@@ -14,19 +12,15 @@ import org.jespxml.JespXML;
 import org.jespxml.modelo.Atributo;
 import org.jespxml.modelo.Tag;
 
-/**
- *
- * @author Patricio Pérez Pinto
- */
-public class Main {
+public class Test {
 
-    /**
-     * @param args the command line arguments
-     */
+    /*
+    Método main para escribir un archivo XML
+    */
     public static void main(String[] args) {
         try {
             //creo el objeto JespXML con el archivo que quiero crear
-            JespXML archivo = new JespXML("clientes.xml");
+            JespXML archivo = new JespXML("pruebaIndent.xml");
             
             //declaro el Tag raiz, que en esta caso se llama bd
             Tag bd = new Tag("bd");
@@ -52,13 +46,40 @@ public class Main {
             //y escribo el archivo XML
             archivo.escribirXML(bd);
         } catch (ParserConfigurationException ex) {
-            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(Test.class.getName()).log(Level.SEVERE, null, ex);
         } catch (TransformerConfigurationException ex) {
-            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(Test.class.getName()).log(Level.SEVERE, null, ex);
         } catch (FileNotFoundException ex) {
-            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(Test.class.getName()).log(Level.SEVERE, null, ex);
         } catch (TransformerException ex) {
-            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(Test.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+    
+    
+    /*
+    Método main para probar la serializacion de objetos con JespXML
+    */
+//    public static void main(String[] args) {
+//        try {
+//            //creo el objeto JespXML con el archivo que quiero crear
+//            JespXML archivo = new JespXML("conf.xml");
+//            
+//            Conf conf = new Conf("BD", "Test");
+//            
+//            archivo.escribir(conf, conf.getClass());
+//            
+//            Conf c = (Conf)archivo.leer(Conf.class);
+//            
+//            System.out.println(c.nombre);
+//            System.out.println(c.valor);
+//            
+//        } catch (FileNotFoundException ex) {
+//            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+//        } catch (JAXBException ex) {
+//            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+//        } catch (IOException ex) {
+//            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//    }
 }
